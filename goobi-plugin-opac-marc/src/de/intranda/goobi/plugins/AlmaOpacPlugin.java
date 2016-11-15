da69bf396b41c0b118b5302ad8ddc37d3f94f08b
Robert Sehr <robert.sehr@intranda.com>
2016-11-15 12:33:15 +0100

    removed unused imports

--- goobi-plugin-opac-marc/src/de/intranda/goobi/plugins/AlmaOpacPlugin.java
+++ goobi-plugin-opac-marc/src/de/intranda/goobi/plugins/AlmaOpacPlugin.java
@@ -2,10 +2,7 @@ package de.intranda.goobi.plugins;
 
 import org.goobi.production.plugin.interfaces.IOpacPlugin;
 
-import de.unigoettingen.sub.search.opac.ConfigOpacCatalogue;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
-import ugh.dl.Fileformat;
-import ugh.dl.Prefs;
 
 @PluginImplementation
 public class AlmaOpacPlugin extends GbvMarcSruImport implements IOpacPlugin {
@@ -16,12 +13,6 @@ public class AlmaOpacPlugin extends GbvMarcSruImport implements IOpacPlugin {
         super.setIdentifierSearchFieldPrefix("other_system_number");
         super.setMarcNamespace(null);
     }
-    
-//    @Override
-//    public Fileformat search(String inSuchfeld, String searchValue, ConfigOpacCatalogue cat, Prefs inPrefs) throws Exception {
-//        
-//        return super.search(inSuchfeld, "other_system_number=" + searchValue, cat, inPrefs);
-//    }
     
     public String getTitle() {
         return "ALMA-MARC";
