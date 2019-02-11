711d236b1238cea1583818b40d23a7d1b7fc8391
Robert Sehr <robert.sehr@intranda.com>
2019-02-11 13:51:55 +0100

    added class for K10plus catalogue

---
+++ goobi-plugin-opac-marc/src/de/intranda/goobi/plugins/K10plusOpacPlugin.java
@@ -0,0 +1,28 @@
+package de.intranda.goobi.plugins;
+
+import org.goobi.production.plugin.interfaces.IOpacPlugin;
+
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+
+@PluginImplementation
+public class K10plusOpacPlugin extends GbvMarcSruImport implements IOpacPlugin {
+
+    public K10plusOpacPlugin() {
+        super();
+        super.setVersion("1.2");
+        super.setIdentifierSearchFieldPrefix("pica.ppn");
+        super.setMarcNamespace(null);
+        super.setSruSchema("marcxml");
+    }
+
+    @Override
+    public String getTitle() {
+        return "K10plus";
+    }
+
+
+    @Override
+    public String getDescription() {
+        return "K10plus";
+    }
+}
